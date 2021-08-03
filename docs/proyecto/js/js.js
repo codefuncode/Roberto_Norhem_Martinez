99968bcb561f31e49c722c809ebb7e9bf3214ad1
alemandiazcarlos <caleman9791>
2021-08-03 10:34:17 -0400

    ok

---
+++ docs/proyecto/js/js.js
@@ -0,0 +1,7 @@
+var imagenes = document.querySelectorAll("img");
+
+console.log(imagenes);
+
+for (var i = 0; i < imagenes.length; i++) {
+	imagenes[i].src = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-xa-1NJxFNFo%2FUl5eBoB2imI%2FAAAAAAAABIA%2FKbxpWz5Nhew%2Fs1600%2Fteclado.jpg&f=1&nofb=1";
+}
